a03a3e52aa764c211ac9d3be91be88b2dd935fa1
arnvald <arnvald.to@gmail.com>
2010-05-22 19:15:12 +0200

    user controller

---
+++ grails-app/controllers/wegetarianice/ApplicationController.groovy
@@ -0,0 +1,17 @@
+package wegetarianice
+
+class ApplicationController {
+
+  def auth = {
+    if(!session.login) {
+      def originalRequestParams = [controller:controllerName, action:actionName]
+        originalRequestParams.putAll(params)
+        session.originalRequestParams = originalRequestParams
+
+      redirect(controller:'user',action:'login')
+      return false
+    }
+    return true
+  }
+}
+
---
+++ grails-app/controllers/wegetarianice/UserController.groovy
@@ -0,0 +1,131 @@
+package wegetarianice
+
+class UserController extends ApplicationController {
+
+  static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+  def beforeInterceptor = [action:this.&auth, except:['login','save','create']]
+
+  def login = {
+    if (request.method == "GET") {
+      session.login = null
+      def user = new User()
+    }
+    else {
+      def user =
+        User.findByLoginAndPassword(params.login,
+                                    params.password)
+        if (user) {
+          session.login = user.login
+          def redirectParams = session.originalRequestParams ?
+                               session.originalRequestParams :
+                               [controller:'user']
+          redirect(controller:'user',action:'index')
+
+        }
+        else {
+          flash['message'] = 'Please enter a valid login and password'
+        }
+     }
+   }
+
+  def logout = {
+    session.userId = null
+    flash['message'] = 'Successfully logged out'
+    redirect(controller:'user', action:'login')
+  }
+
+
+  def index = {
+    redirect(action: "list", params: params)
+  }
+
+  def list = {
+    params.max = Math.min(params.max ? params.int('max') : 10, 100)
+    [userInstanceList: User.list(params), userInstanceTotal: User.count()]
+  }
+
+  def create = {
+    def userInstance = new User()
+    userInstance.properties = params
+    return [userInstance: userInstance]
+  }
+
+  def save = {
+    def userInstance = new User(params)
+    if (userInstance.save(flush: true)) {
+      flash.message = "${message(code: 'default.created.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])}"
+      redirect(action: "show", id: userInstance.id)
+    }
+    else {
+      render(view: "create", model: [userInstance: userInstance])
+    }
+  }
+
+  def show = {
+    def userInstance = User.get(params.id)
+    if (!userInstance) {
+      flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+      redirect(action: "list")
+    }
+    else {
+      [userInstance: userInstance]
+    }
+  }
+
+  def edit = {
+    def userInstance = User.get(params.id)
+    if (!userInstance) {
+      flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+      redirect(action: "list")
+    }
+    else {
+      return [userInstance: userInstance]
+    }
+  }
+
+  def update = {
+    def userInstance = User.get(params.id)
+      if (userInstance) {
+        if (params.version) {
+        def version = params.version.toLong()
+        if (userInstance.version > version) {
+          userInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'user.label', default: 'User')] as Object[], "Another user has updated this User while you were editing")
+          render(view: "edit", model: [userInstance: userInstance])
+          return
+        }
+      }
+      userInstance.properties = params
+      if (!userInstance.hasErrors() && userInstance.save(flush: true)) {
+        flash.message = "${message(code: 'default.updated.message', args: [message(code: 'user.label', default: 'User'), userInstance.id])}"
+        redirect(action: "show", id: userInstance.id)
+      }
+      else {
+        render(view: "edit", model: [userInstance: userInstance])
+      }
+    }
+    else {
+        flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+        redirect(action: "list")
+    }
+  }
+
+  def delete = {
+    def userInstance = User.get(params.id)
+    if (userInstance) {
+      try {
+        userInstance.delete(flush: true)
+        flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+        redirect(action: "list")
+      }
+      catch (org.springframework.dao.DataIntegrityViolationException e) {
+        flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+        redirect(action: "show", id: params.id)
+      }
+  }
+  else {
+    flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'user.label', default: 'User'), params.id])}"
+    redirect(action: "list")
+    }
+  }
+}
