3c9634c54207484552af73a80acb2ca45cbb7713
arnvald <arnvald.to@gmail.com>
2010-06-04 10:38:18 +0200

    urlmapping

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,14 +1,22 @@
 class UrlMappings {
     static mappings = {
-    "/admin/user/$action?/$id?"(controller:"adminUser")
+    "/admin/article/$action?/$id?"(controller:"adminArticle")
+    "/admin/articleCategory/$action?/$id?"(controller:"adminArticleCategory")
+    "/admin/comment/$action?/$id?"(controller:"adminComment")
+    "/admin/food/$action?/$id?"(controller:"adminFood")
+    "/admin/foodCategory/$action?/$id?"(controller:"adminFoodCategory")
     "/admin/page/$action?/$id?"(controller:"adminPage")
     "/admin/recipe/$action?/$id?"(controller:"adminRecipe")
+    "/admin/repiceCategory/$action?/$id?"(controller:"adminRecipeCategory")
+    "/admin/unit/$action?/$id?"(controller:"adminUnit")
+    "/admin/user/$action?/$id?"(controller:"adminUser")
       "/$controller/$action?/$id?"{
 	      constraints {
 			 // apply constraints here
 		  }
 	  }
       "/"(view:"/index")
+      "/test"(view:"/testlayout")
 	  "500"(view:'/error')
 	}
 }
