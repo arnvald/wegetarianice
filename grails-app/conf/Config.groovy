afcfb51cb8d0a1e8b4853a1fa312a914ba304e88
arnvald <arnvald.to@gmail.com>
2010-06-12 13:25:13 +0200

    jquery

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -29,6 +29,7 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 // The default codec used to encode data with ${}
 grails.views.default.codec="none" // none, html, base64
 grails.views.gsp.encoding="UTF-8"
+grails.views.javascript.library="jquery"
 grails.converters.encoding="UTF-8"
 // enable Sitemesh preprocessing of GSP pages
 grails.views.gsp.sitemesh.preprocess = true
