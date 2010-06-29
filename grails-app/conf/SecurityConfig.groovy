1587bece6bfe531a81690a4e0683de46bad0c4e7
Filip <filip@filip-laptop.(none)>
2010-06-29 23:06:02 +0200

    blad

--- grails-app/conf/SecurityConfig.groovy
+++ grails-app/conf/SecurityConfig.groovy
@@ -17,7 +17,6 @@
         //a tyklo zalogowani to pewnych szczegolnych wlasciwosci zwiazanych ze swoim panelem
 
         requestMapString = '''CONVERT_URL_TO_LOWERCASE_BEFORE_COMPARISON
-<<<<<<< HEAD:grails-app/conf/SecurityConfig.groovy
      PATTERN_TYPE_APACHE_ANT
 
     /artykuly/nowy/**=IS_AUTHENTICATED_REMEMBERED
