2a9c5b0b6aec2b5a46ef1cf3db76a0b566573636
Filip <filip@filip-laptop.(none)>
2010-06-29 23:04:37 +0200

    logowaine

--- grails-app/conf/SecurityConfig.groovy
+++ grails-app/conf/SecurityConfig.groovy
@@ -7,6 +7,8 @@
 	loginUserDomainClass = "wegetarianice.User"
 	authorityDomainClass = "wegetarianice.Role"
 	requestMapClass = "wegetarianice.Requestmap" //chyba niepotrzebne skoro zrobilismy to z palca
+        authenticationFailureUrl="/zaloguj"
+        loginFormUrl="/zaloguj"
 
         //apropo pisania z palca to chyba japrosciej bedzie
         //jak wezmiemy ze w ogolnosci wszyscy maja dostep do wszystkiego
